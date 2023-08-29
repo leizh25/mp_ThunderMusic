c80eb5ffb4353d29cc53b50ff022053cff7e37b9
leizh25 <leizhuzheng@qq.com>
2023-08-29 23:09:56 +0800

    自定义组件

---
+++ components/NavHeader/NavHeader.js
@@ -0,0 +1,30 @@
+// components/NavHeader/NavHeader.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        title:{
+            type:String,
+            value:"默认标题"
+        },
+        nav:{
+            type:String,
+            value:"我是描述"
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
