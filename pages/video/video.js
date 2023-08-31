9eeaa2a72b0dd9fdc8e0ca39c14d927dcc7b79ab
leizh25 <leizhuzheng@qq.com>
2023-09-01 00:04:39 +0800

    导航数据动态展示

--- pages/video/video.js
+++ pages/video/video.js
@@ -1,18 +1,36 @@
 // pages/video/video.js
+import request from "../../utils/request"
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        videoGroupList: [],
+        navId: ""
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-
+        this.getVideoGroupListData()
+    },
+    //获取导航数据,
+    async getVideoGroupListData() {
+        let videoGroupListData = await request("/video/group/list")
+        console.log(videoGroupListData.data[0].id)
+        this.setData({
+            videoGroupList: videoGroupListData.data.slice(0, 14),
+            navId: videoGroupListData.data[0].id,
+        })
+    },
+    //点击切换导航的回调
+    changeNav(e) {
+        let navId = e.currentTarget.id
+        this.setData({
+            navId: navId * 1 //转数字类型
+        })
     },
 
     /**
