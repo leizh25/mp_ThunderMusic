5932b3ac35a382b7e17139559ffde60d553cf076
leizh25 <leizhuzheng@qq.com>
2023-09-05 19:37:36 +0800

    转发分享功能实现

--- pages/video/video.js
+++ pages/video/video.js
@@ -10,7 +10,8 @@ Page({
         navId: "", //导航的标识
         videoList: [], //视频列表数据
         isTriggered: false, //标识下拉刷新是否被触发
-        offset: 0, //视频分页
+        offset: 0, //视频分页,
+        isLoading: false, //是否正在加载
 
     },
 
@@ -33,6 +34,9 @@ Page({
     },
     //获取视频列表数据
     async getVideoList(navId, offset = this.data.offset) {
+        this.setData({
+            isLoading: true
+        })
         let videoDetails = await request('/video/group', {
             id: navId,
             offset
@@ -60,7 +64,8 @@ Page({
         this.setData({
             videoList: this.data.videoList.concat(videoInfoList),
             //关闭下拉刷新
-            isTriggered: false
+            isTriggered: false,
+            isLoading: false
         })
         //关闭消息提示框
         wx.hideLoading()
@@ -107,7 +112,7 @@ Page({
         // console.log("scroll-view下拉刷新");
         // 再次发请求,获取最新的视频列表数据
         this.setData({
-            videoList:[]
+            videoList: []
         })
         this.getVideoList(this.data.navId)
     },
@@ -115,6 +120,7 @@ Page({
     handleToLower() {
         // console.log("scroll-view上拉触底");
         //数据分页: 1.后端分页  2.前端分页
+        if (this.data.isLoading) return
         console.log("发情请求 || 在前端截取最新的数据 追加到视频列表的后方");
         let tempOffset = this.data.offset
         this.getVideoList(this.data.navId, tempOffset + 1).then(() => {
@@ -156,20 +162,34 @@ Page({
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh() {
-
+        console.log("页面的下拉刷新");
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
     onReachBottom() {
-
+        console.log("页面的下拉触底");
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage() {
+    onShareAppMessage({from}) {
+        console.log('from: ', from);
+        if (from == "button") {
+            return {
+                title: "来自button的转发",
+                page: "/pages/video/video",
+                imageUrl: "/static/images/nvsheng.jpg"
+            }
+        } else {
+            return {
+                title: "来自menu的转发",
+                page: "/pages/video/video",
+                imageUrl: "/static/images/nvsheng.jpg"
+            }
+        }
 
     }
 })
